babe2c7a658a7411a65511c5429a46477084a8b0
Абдуллина Язиля <A_250494@mail.ru>
2022-08-05 21:12:06 +0300

    Тесты падают

--- src/test/java/ru/netology/rest/PostmanEchoTest.java
+++ src/test/java/ru/netology/rest/PostmanEchoTest.java
@@ -20,7 +20,7 @@ public class PostmanEchoTest {
 
                 .then()
                 .statusCode(200)
-                .body("data", equalTo("some data"))
+                .body("data", equalTo("some value"))
         ;
     }
 
@@ -37,7 +37,7 @@ public class PostmanEchoTest {
 
                 .then()
                 .statusCode(200)
-                .body("data", equalTo("Добрового времени суток!"))
+                .body("data", equalTo("Привет!"))
         ;
     }
 }
